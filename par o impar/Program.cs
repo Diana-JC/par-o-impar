e0ab56a3ab6d40d84dce7bd5060eec323ef5662c
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 19:14:46 -0400

    Agregar archivos de proyecto.

---
+++ par o impar/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    internal class Program
+{
+    static void Main(string[] args)
+
+    { // valor de entrada que se le solicita al usuario
+        int numero;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = int.Parse(Console.ReadLine());
+
+        // proceso para saber si el numero es par o impar
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero que ingreso es par");
+
+        }
+        else
+        {
+            Console.WriteLine("El numero ingresado es impar");
+        }
+
+        Console.ReadKey();
+    }
+}
+
